539eb6bc839dba363edbc00a24882e60a923e908
Ömer Okumuş <omerokumus3@gmail.com>
2024-09-15 10:05:35 +0300

    defer

--- SwiftLearning/main.swift
+++ SwiftLearning/main.swift
@@ -9,3 +9,45 @@ import Foundation
 
 print("Hello, World!")
 
+
+func fetchHardwareStatus() {
+    
+}
+
+func fetchSoftwareStatus() {
+    
+}
+
+func fetchNetworkStatus() {
+    
+}
+
+func openFile() {
+    
+}
+
+func closeFile(_ file: Void) {
+    
+}
+
+
+
+func writeLog() {
+    let file = openFile()
+    defer {
+        closeFile(file)
+    }
+    
+    let hardwareStatus = fetchHardwareStatus()
+    guard hardwareStatus != "disaster" else { return }
+    file.write(hardwareStatus)
+    
+    let softwareStatus = fetchSoftwareStatus()
+    guard softwareStatus != "disaster" else { return }
+    file.write(softwareStatus)
+    
+    let networkStatus = fetchNetworkStatus()
+    guard neworkStatus != "disaster" else { return }
+    file.write(networkStatus)
+}
+
