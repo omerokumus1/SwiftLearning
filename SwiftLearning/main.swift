2c208c97ef9e7143e229ffdd71a762dacc9175bd
Ömer Okumuş <omerokumus3@gmail.com>
2024-09-15 09:54:16 +0300

    repeating func

--- SwiftLearning/main.swift
+++ SwiftLearning/main.swift
@@ -9,3 +9,13 @@ import Foundation
 
 print("Hello, World!")
 
+func repeating(times: Int, block: (Int) -> Void) {
+    for i in 0..<times {
+        block(i)
+    }
+}
+
+repeating(times: 2) { i in
+    print(i)
+}
+
