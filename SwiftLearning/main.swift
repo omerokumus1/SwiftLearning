d36ab0063e78dee75170e01994a55ed7af7d70f5
Ömer Okumuş <omerokumus3@gmail.com>
2024-11-12 11:02:17 +0300

    global variables

--- SwiftLearning/main.swift
+++ SwiftLearning/main.swift
@@ -7,5 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
+// Global Computed Variables
+var currentDateTime: String {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .medium
+    return formatter.string(from: Date())
+}
+
+print(currentDateTime)
+
+// Global Read-Only Computed Variables
+var pi: Double {
+    return 3.14159
+}
+
+// Global Read-Write Computed Variables
+var temperatureInCelsius: Double = 20.0
+
+var temperatureInFahrenheit: Double {
+    get {
+        return (temperatureInCelsius * 9 / 5) + 32
+    }
+    set {
+        temperatureInCelsius = (newValue - 32) * 5 / 9
+    }
+}
 
